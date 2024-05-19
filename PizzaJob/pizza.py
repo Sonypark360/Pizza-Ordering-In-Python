0fabfd71b3cf0756e767e7994a43b70513c8a4ac
Sonypark360 <sunnyobinna@gmail.com>
2024-05-19 13:45:53 -0600

    Latest

--- PizzaJob/pizza.py
+++ PizzaJob/pizza.py
@@ -2,7 +2,9 @@ print("Thank you for choosing Python Pizza Deliveries!")
 size = input("What size of pizza do you want? S, M, or L \n")
 add_pepperoni = input("Do you want pepperoni? Y or N \n")
 extra_cheese = input("Do you want extra cheese? Y or N \n")
+
 price = int(0)
+
 if size == "S" :
    price = int(15) 
 if size == "M" :
@@ -19,4 +21,5 @@ if add_pepperoni == "Y" :
      price += 3
 if extra_cheese == "Y" :
    price += 1
+   
 print(f"Thank you for choosing Python Pizza Deliveries! Your final bill is: {price}")
